e5ff3424942a335d66f74e98b00ee04e31124c5b
mprey <masonprey7@gmail.com>
2026-07-20 17:19:47 -0400

    fix: remove setval

--- cdk/lambda/db-init/migrations/012_strategy_id_identity.sql
+++ cdk/lambda/db-init/migrations/012_strategy_id_identity.sql
@@ -1,5 +1,2 @@
 ALTER TABLE strategy.strategy
   ALTER COLUMN strategy_id ADD GENERATED ALWAYS AS IDENTITY;
-
-SELECT setval(pg_get_serial_sequence('strategy.strategy', 'strategy_id'),
-              COALESCE((SELECT MAX(strategy_id) FROM strategy.strategy), 0));
